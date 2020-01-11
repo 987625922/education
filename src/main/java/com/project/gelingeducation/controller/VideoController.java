2ca733a1aa7a7c04dd332a52aafc457a874e3917
wind <987625922@qq.com>
2020-01-11 17:52:06 +0800

    添加一些接口请求限制为post方法

--- src/main/java/com/project/gelingeducation/controller/VideoController.java
+++ src/main/java/com/project/gelingeducation/controller/VideoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("video")
+@RequestMapping("/video")
 @RestController
 @Slf4j
 public class VideoController {
